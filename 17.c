e4df350696b28f2aab2cf7eb36e82b547e72bc11
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2023-01-23 13:01:31 +0600

    Print the odd number from 1 to n

---
+++ 17.c
@@ -0,0 +1,15 @@
+//Print the odd numbers from 1 to n in C
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++) {
+        if (i % 2 != 0) {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
