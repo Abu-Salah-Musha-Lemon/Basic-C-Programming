c1a70839af9b1ca2c0901b22df760d561534260e
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2023-01-23 14:04:35 +0600

    1*2*3*........................*m display the serise  and calculate

---
+++ 24.c
@@ -0,0 +1,16 @@
+// 1*2*3*........................*m display the serise  and calculate in c
+
+#include <stdio.h>
+
+int main() {
+    int m, i;
+    int product=1;
+    printf("Enter a number: ");
+    scanf("%d", &m);
+    for (i = 1; i <= m; i++) {
+        printf("%d*", i);
+        product *= i;
+    }
+    printf("\b = %d\n", product);
+    return 0;
+}
