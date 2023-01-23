3226a4359f77b20197ff7a1a7b1183482f1d8567
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2023-01-23 12:55:43 +0600

    use two more loop
    Uses a while loop to print "Ict" a certain number of times
    
    Uses a do while loop to print "Ict" a certain number of times

--- 16.c
+++ 16.c
@@ -11,3 +11,34 @@ int main() {
     }
     return 0;
 }
+
+
+//Uses a while loop to print "Ict" a certain number of times:
+
+int main() {
+    int n, i=1;
+    printf("Enter the number of times you want to print 'Ict': ");
+    scanf("%d", &n);
+    while(i<=n){
+        printf("Ict\n");
+        i++;
+    }
+    return 0;
+}
+
+//Uses a do while loop to print "Ict" a certain number of times:
+
+#include <stdio.h>
+
+int main() {
+    int n, i=1;
+    printf("Enter the number of times you want to print 'Ict': ");
+    scanf("%d", &n);
+    do{
+        printf("Ict\n");
+        i++;
+    }while(i<=n);
+
+    return 0;
+}
+
