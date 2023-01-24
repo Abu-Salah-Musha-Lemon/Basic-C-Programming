bca551e07d62ccfac67e0204eca589926fc56af4
Abu-Salah-Musha-Lemon <lemonahmed512@gmail.com>
2023-01-24 11:53:35 +0600

    (1+2)*(2+3)*(3*4)........................*m display the series  and calculate sum

---
+++ 28.c
@@ -0,0 +1,18 @@
+
+//(1+2)*(2+3)*(3*4)........................*m display the series  and calculate sum
+// (1+2)*(2+3)*(3*4)........................*(m+n) display the series  and calculate sum
+
+#include <stdio.h>
+
+int main() {
+    int m;
+    int product=1;
+    printf("Enter the value of m: ");
+    scanf("%d", &m);
+    for (int i = 1; i <= m; i++) {
+        printf("(%d+%d)*", i,i+1);
+        product *= (i+(i+1));
+    }
+    printf("\b\b = %d\n", product);
+    return 0;
+}
